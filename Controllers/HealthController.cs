f2142d3eaa17ebf8bd6a12c5fe6c9a2eead6f2f1
henriquegomesteixeira <henriquegomes080012@gmail.com>
2025-06-19 02:03:01 -0300

    Remoção da configuração do Fly.io e adição do endpoint de verificação de integridade

---
+++ Controllers/HealthController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace everdadeisso.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HealthController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get() => Ok("OK");
+    }
+}
